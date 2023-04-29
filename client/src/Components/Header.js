4946d7a52b240101a5fb0a9ca0308002e50abb0d
home <home@MacBook-Pro-home.local>
2023-04-29 19:20:41 +0300

    Buttons

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -4,21 +4,44 @@ import githubSVG from "../Assets/github.svg";
 import linkedinSVG from "../Assets/linkedin.svg";
 
 export default function Header() {
+  const scrollToContact = () =>
+  {
+    window.scrollTo({
+      top: document.getElementsByClassName("contact")[0].offsetTop,
+      behavior: "smooth",
+    })
+  }
+  
+  const openInstagram = () =>
+  {
+    window.open("https://www.instagram.com/catalin__pop");
+  }
+
+  const openLinkedin = () =>
+  {
+    window.open("https://www.linkedin.com/in/catalin-pop-7440b0261/");
+  }
+
+  const openGithub = () =>
+  {
+    window.open("https://github.com/CatalinP0P");
+  }
+
   return (
     <div>
 
         <div className='header container-1'>
             <div className='row headerLeft' >
                 <label className='h4Design' >Pop Catalin</label>
-                <p className='headerOptionals'> About me </p>
-                <p className='headerOptionals' > Contact </p>
+                <p className='headerOptionals' > About me </p>
+                <p className='headerOptionals' onClick={() => scrollToContact()} > Contact </p>
             </div>
             
             <div className='row headerRight' >
                 <p className='color-secondary headerOptionals' > (0746) 816 304 </p>
-                <img src={instagramSVG}></img>
-                <img src={githubSVG} ></img>
-                <img src={linkedinSVG} ></img>
+                <img src={instagramSVG} onClick={() => openInstagram()} ></img>
+                <img src={githubSVG} onClick={() => openGithub()}></img>
+                <img src={linkedinSVG} onClick={() => openLinkedin()} ></img>
             </div>
         </div>
         <hr className='color-black-25' ></hr>
--- client/src/Components/Hero.js
+++ client/src/Components/Hero.js
@@ -3,12 +3,20 @@ import "./Hero.css";
 import downSVG from "../Assets/downarrow.svg";
 
 export default function Hero() {
+  const handleScroll = () =>
+  {
+    window.scrollTo({
+      top: document.getElementsByClassName("myWork")[0].offsetTop,
+      behavior: "smooth",
+    });
+  }
+
   return (
     <div className='heroArea container-2' >
         <h4 className='color-primary' > Hi there! </h4>
         <h3> I'm Pop Catalin. </h3>
         <p className='color-black-50' >I am a self-taught web developer who’s looking for endless improvement.<br/>Welcome to my portfolio web page</p>
-        <button className='btn btn-primary bg-gradient' >
+        <button className='btn btn-primary bg-gradient' onClick={() => handleScroll()} >
           see my work
           <img src={downSVG} ></img>
         </button>
